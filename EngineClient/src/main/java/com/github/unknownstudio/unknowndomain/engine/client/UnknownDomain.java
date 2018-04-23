00ae4f8d8381dd9412883287ba7e3aabf5dbf097
Mouse <937279156@qq.com>
2018-04-24 00:48:53 +0800

    Update UnknownDomain

--- EngineClient/src/main/java/com/github/unknownstudio/unknowndomain/engine/client/UnknownDomain.java
+++ EngineClient/src/main/java/com/github/unknownstudio/unknowndomain/engine/client/UnknownDomain.java
@@ -1,6 +1,10 @@
 package com.github.unknownstudio.unknowndomain.engine.client;
 
 public class UnknownDomain {
+	
+	public static final String NAME = "${project.name}";
+	public static final String VERSION = "${project.version";
+	
     public static final int WIDTH = 550, HEIGHT = 400;
     private static GameMain main;
 
