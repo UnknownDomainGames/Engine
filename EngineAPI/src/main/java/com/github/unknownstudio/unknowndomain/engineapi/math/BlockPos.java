a3c46b1801999540e6ea566964cd379f55da50c9
Mouse <937279156@qq.com>
2018-05-27 14:45:42 +0800

    Update BlockPos

--- EngineAPI/src/main/java/com/github/unknownstudio/unknowndomain/engineapi/math/BlockPos.java
+++ EngineAPI/src/main/java/com/github/unknownstudio/unknowndomain/engineapi/math/BlockPos.java
@@ -1,9 +1,54 @@
 package com.github.unknownstudio.unknowndomain.engineapi.math;
 
-public class BlockPos extends Vector3d {
+public final class BlockPos {
+	
+	public static final BlockPos ZERO = new BlockPos(0, 0, 0);
+	
+	private final int x,y,z;
 
-    public BlockPos(double x, double y, double z) {
-        super(x, y, z);
+	public BlockPos(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+	
+    public BlockPos add(int x, int y, int z){
+    	return new BlockPos(this.x + x, this.y + y, this.z + z);
     }
 
+    public BlockPos minus(int x, int y, int z){
+    	return new BlockPos(this.x - x, this.y - y, this.z - z);
+    }
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof BlockPos))
+			return false;
+		
+		BlockPos others = (BlockPos) obj;
+		
+		return x == others.x && y == others.y && z == others.z;
+	}
+	
+	@Override
+	public int hashCode() {
+		return (x * 31 + y) * 31 + z;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("BlockPos(%i,%i,%i)", x, y, z);
+	}
 }
