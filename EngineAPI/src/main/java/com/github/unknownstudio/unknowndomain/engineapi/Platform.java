e876248b9affe9fdf6cfc367274ea663f0d2cb35
cvrunmin <lung1a16@hotmail.com>
2018-06-16 23:38:51 +0800

    events

---
+++ EngineAPI/src/main/java/com/github/unknownstudio/unknowndomain/engineapi/Platform.java
@@ -0,0 +1,10 @@
+package com.github.unknownstudio.unknowndomain.engineapi;
+
+import com.github.unknownstudio.unknowndomain.engineapi.util.Logger;
+
+/**
+ * Engine Platform
+ */
+public class Platform {
+    public static final Logger LOGGER = new Logger();
+}
---
+++ EngineAPI/src/main/java/com/github/unknownstudio/unknowndomain/engineapi/mod/ModLoadListener.java
@@ -0,0 +1,5 @@
+package com.github.unknownstudio.unknowndomain.engineapi.mod;
+
+public interface ModLoadListener {
+    void onModLoaded(ModContainer mod);
+}
