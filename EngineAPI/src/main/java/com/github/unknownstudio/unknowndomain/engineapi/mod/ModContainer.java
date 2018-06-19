9b40ea910a6dfb77a9f3465ee36c7c2af6de51e6
Mouse <937279156@qq.com>
2018-06-19 23:37:04 +0800

    Update Mod

--- EngineAPI/src/main/java/com/github/unknownstudio/unknowndomain/engineapi/mod/ModContainer.java
+++ EngineAPI/src/main/java/com/github/unknownstudio/unknowndomain/engineapi/mod/ModContainer.java
@@ -3,15 +3,36 @@ package com.github.unknownstudio.unknowndomain.engineapi.mod;
 import com.github.unknownstudio.unknowndomain.engineapi.util.versioning.ComparableVersion;
 
 public class ModContainer {
-    private String modid;
-
-    private ComparableVersion version;
-
+	
+    private final String modid;
+    private final ComparableVersion version;
+    
     private String name;
+    
+    private Object instance;
 
-    public ModContainer(String modid, String name, String version){
+    public ModContainer(String modid, String version){
         this.modid = modid;
-        this.name = name;
         this.version = new ComparableVersion(version);
     }
+
+	public String getModid() {
+		return modid;
+	}
+
+	public ComparableVersion getVersion() {
+		return version;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Object getInstance() {
+		return instance;
+	}
 }
