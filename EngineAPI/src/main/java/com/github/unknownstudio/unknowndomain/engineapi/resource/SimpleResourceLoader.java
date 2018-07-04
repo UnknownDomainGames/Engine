1ee3d3a2e08edb5aeef7a767e3a216629f7626ee
XiaoLeGG <786071433@qq.com>
2018-07-04 22:45:25 +0800

    Fix a small problem

--- EngineAPI/src/main/java/com/github/unknownstudio/unknowndomain/engineapi/resource/SimpleResourceLoader.java
+++ EngineAPI/src/main/java/com/github/unknownstudio/unknowndomain/engineapi/resource/SimpleResourceLoader.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import javax.annotation.Nonnull;
+
 public class SimpleResourceLoader extends ResourceLoader{
 
 	public SimpleResourceLoader() {}
 	@Override
-	public final InputStream getResourceStream(URL url) throws IOException{
+	public final InputStream getResourceStream(@Nonnull URL url) throws IOException{
 		return url.openConnection().getInputStream();
 	}
 
