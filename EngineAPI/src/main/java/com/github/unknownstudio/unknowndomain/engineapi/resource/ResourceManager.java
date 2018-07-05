3689f00c8701969e94a5b42834aff30944653985
XiaoLeGG <786071433@qq.com>
2018-07-06 00:19:31 +0800

    Update

---
+++ EngineAPI/src/main/java/com/github/unknownstudio/unknowndomain/engineapi/resource/ResourceManager.java
@@ -0,0 +1,5 @@
+package com.github.unknownstudio.unknowndomain.engineapi.resource;
+
+public abstract class ResourceManager {
+
+}
---
+++ EngineAPI/src/main/java/com/github/unknownstudio/unknowndomain/engineapi/resource/ResourceReceiver.java
@@ -0,0 +1,8 @@
+package com.github.unknownstudio.unknowndomain.engineapi.resource;
+
+public interface ResourceReceiver {
+	/**
+	 * Update when ResourceManager update
+	 */
+	public void update(Resource resource);
+}
