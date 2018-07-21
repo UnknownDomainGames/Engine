c0b2504bfb46726240c86d2ee0c72f253dda4fba
Mouse <937279156@qq.com>
2018-07-21 22:34:14 +0800

    Update permission

---
+++ EngineAPI/src/main/java/unknowndomain/engine/api/permission/Permissable.java
@@ -0,0 +1,10 @@
+package unknowndomain.engine.api.permission;
+
+public interface Permissable {
+
+	boolean hasPermission(String permission);
+	
+	void addPermission(String permission);
+	
+	boolean removePermission(String permission);
+}
