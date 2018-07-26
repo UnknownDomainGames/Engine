8a0a0391e7b138ffc2c7b0cc878b9849cd5b04eb
Mouse <937279156@qq.com>
2018-07-26 21:00:36 +0800

    Update Permission

--- EngineAPI/src/main/java/unknowndomain/engine/api/permission/PermissionProvider.java
+++ EngineAPI/src/main/java/unknowndomain/engine/api/permission/PermissionProvider.java
@@ -1,49 +1,50 @@
 package unknowndomain.engine.api.permission;
 
 import java.util.HashMap;
+import java.util.Map;
 
-public class PermissionProvider implements PermissionAPI{
-	protected HashMap<Permissable,HashMap<String,Boolean>> persimissionMap;
-	private String wildCard="*";//Í¨Åä·û
-	private String splitChar=".";//·Ö¸ô·û
+public class PermissionProvider implements PermissionAPI {
 	
+	public static final String WILDCARD = "*";
+	public static final char SPLITTER = '.';
+	
+	private final Map<Permissable, Map<String, Boolean>> permissableToPermissions;
+
 	public PermissionProvider() {
-		persimissionMap=new HashMap<>();
+		permissableToPermissions = new HashMap<>();
 	}
-	
+
 	@Override
-	public boolean hasPermission(Permissable persimissable, String persimission) {
-		HashMap<String,Boolean> map=persimissionMap.get(persimissable);
-		if(map==null) {
-			persimissionMap.put(persimissable, new HashMap<>());
+	public boolean hasPermission(Permissable permissable, String permission) {
+		Map<String, Boolean> permissions = permissableToPermissions.get(permissable);
+		if (permissions == null) {
+			permissableToPermissions.put(permissable, new HashMap<>());
 			return false;
 		}
-		Boolean value=map.get(persimission);
-		if(value!=null&&value)return true;
-		else {
-			String[] strs=persimission.split("\\"+splitChar);
-			persimission=strs[0];
-			int temp=2;
-			while(temp<=strs.length) {
-				for(int i=1;i<=strs.length-temp;i++)persimission=persimission+splitChar+strs[i];
-				String tempStr=persimission+splitChar+wildCard;
-				if(map.containsKey(tempStr)) {
-					if(map.get(tempStr))
-					return true;
-				}
-				temp++;
-			}
-			return false;
+
+		while (true) {
+			Boolean value = permissions.get(permission);
+			if (value != null)
+				return value;
+
+			if (permission.isEmpty())
+				return false;
+
+			int lastSplitIndex = permission.lastIndexOf(SPLITTER);
+			permission = lastSplitIndex == -1 ? WILDCARD : permission.substring(0, lastSplitIndex);
 		}
 	}
 
 	@Override
-	public void setPermission(Permissable persimissable, String persimission, boolean value) {
-		HashMap<String,Boolean> map=persimissionMap.get(persimissable);
-		if(map==null) {
-			map=new HashMap<>();
-			persimissionMap.put(persimissable, map);
+	public void setPermission(Permissable permissable, String permission, boolean value) {
+		Map<String, Boolean> permissions = permissableToPermissions.get(permissable);
+		if (permissions == null) {
+			permissions = new HashMap<>();
+			permissableToPermissions.put(permissable, permissions);
 		}
-		map.put(persimission, value);
+		
+		permissions.put(permission.endsWith(SPLITTER + WILDCARD)
+				? permission.substring(0, permission.length() - 2) // Handle wildcard. "permission.*" -> "permission".
+				: permission, value);
 	}
 }
