c501d23bad4acedcf020f5aa85984a6e7cb0e4a5
ci010 <cijhn@hotmail.com>
2018-07-31 18:46:55 +0800

    add swap

---
+++ EngineClient/src/main/java/unknowndomain/engine/client/util/GLSwap.java
@@ -0,0 +1,37 @@
+package unknowndomain.engine.client.util;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.opengl.GL31.*;
+import static org.lwjgl.opengl.GL43.*;
+
+
+public class GLSwap {
+
+    class Mapping {
+        int vbo;
+
+        int srcOffset;
+        int destOffset;
+
+        int length;
+    }
+
+    public static void remap(Mapping[] mappings) {
+        int length = 0;
+        for (Mapping m : mappings) {
+            length += m.length;
+        }
+
+        int dest = glGenBuffers(); // create dest buffer
+        glBindBuffer(GL_ARRAY_BUFFER, dest);
+        nglBufferData(GL_ARRAY_BUFFER, length, 0, GL_STATIC_DRAW); // init with empty and correct size
+
+        for (Mapping m : mappings) {
+            glCopyBufferSubData(m.vbo, dest, m.srcOffset, m.destOffset, m.length);
+        }
+    }
+}
