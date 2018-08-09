bafa529e1cbe2986c9039a77b8913c00835e3aeb
Mouse <937279156@qq.com>
2018-08-10 00:36:14 +0800

    Added comments for Facing

--- EngineAPI/src/main/java/unknowndomain/engine/util/Facing.java
+++ EngineAPI/src/main/java/unknowndomain/engine/util/Facing.java
@@ -2,19 +2,37 @@ package unknowndomain.engine.util;
 
 public enum Facing {
 
+	/**
+	 * Positive x-axis
+	 */
 	NORTH(1),
+	/**
+	 * Negative x-axis
+	 */
 	SOUTH(0),
+	/**
+	 * Positive z-axis
+	 */
 	EAST(3),
+	/**
+	 * Negative z-axis
+	 */
 	WEST(2),
+	/**
+	 * Positive y-axis
+	 */
 	TOP(5),
+	/**
+	 * Negative y-axis
+	 */
 	BOTTOM(4);
-	
+
 	private final int opposite;
-	
-	Facing(int opposite){
+
+	Facing(int opposite) {
 		this.opposite = opposite;
 	}
-	
+
 	public Facing opposite() {
 		return values()[opposite];
 	}
