ba7d077502dd466d09f98cf7cb8d3e4e913fd716
Mouse <937279156@qq.com>
2018-10-10 21:50:41 +0800

    Update Block

--- EngineAPI/src/main/java/unknowndomain/engine/block/Block.java
+++ EngineAPI/src/main/java/unknowndomain/engine/block/Block.java
@@ -13,7 +13,7 @@ import unknowndomain.engine.world.World;
 public interface Block extends RegistryEntry<Block>, RuntimeObject, BlockPrototype.PlaceBehavior, BlockPrototype.ActiveBehavior, BlockPrototype.DestroyBehavior, BlockPrototype.TouchBehavior {
     // think about blockstate and tileentity...
 
-    AABBd MAX_BOUNDING_BOX = new AABBd(0, 0, 0, 1, 1, 1);
+    AABBd DEFAULT_BOUNDING_BOX = new AABBd(0, 0, 0, 1, 1, 1);
 
     ImmutableMap<BlockPrototype.Property<?>, Comparable<?>> getProperties();
 
