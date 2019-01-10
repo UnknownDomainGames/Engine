02b07c9ad448a0ff781b045f6c247b0551671d14
Mouse <mouse0w0@qq.com>
2019-01-10 21:27:10 +0800

    Remove unuse classes

--- EngineAPI/src/main/java/unknowndomain/engine/permission/Permissable.java
+++
@@ -1,9 +0,0 @@
-package unknowndomain.engine.permission;
-
-public interface Permissable {
-	boolean hasPermission(String permission);
-	
-	void setPermission(String permission, boolean value);
-	
-	boolean removePermission(String permission);
-}
--- EngineAPI/src/main/java/unknowndomain/engine/permission/PermissionManager.java
+++
@@ -1,8 +0,0 @@
-package unknowndomain.engine.permission;
-
-public interface PermissionManager {
-	
-	public boolean hasPermission(Permissable permissable, String permission);
-
-	public void setPermission(Permissable permissable, String permission, boolean value);
-}
