c70d4789e18189ef23d519fdd505e0157530c897
Mouse <mouse0w0@qq.com>
2019-01-20 01:03:31 +0800

    Prepare BlockModelBaker

--- EngineAPI/src/main/java/unknowndomain/engine/client/rendering/block/model/BakedBlockModel.java
+++ EngineAPI/src/main/java/unknowndomain/engine/client/rendering/block/model/BakedBlockModel.java
@@ -1,11 +1,50 @@
 package unknowndomain.engine.client.rendering.block.model;
 
+import unknowndomain.engine.client.block.ClientBlock;
+import unknowndomain.engine.client.rendering.block.BlockMeshGenerator;
+import unknowndomain.engine.client.rendering.util.BufferBuilder;
+import unknowndomain.engine.math.BlockPos;
 import unknowndomain.engine.util.Facing;
+import unknowndomain.engine.world.BlockAccessor;
 
-import javax.annotation.Nullable;
+import java.util.List;
 
-// TODO: Implement it.
-public interface BakedBlockModel {
+public class BakedBlockModel implements BlockMeshGenerator {
 
-    float[] getFaceData(@Nullable Facing facing);
+    private final List<int[]> meshes;
+
+    public BakedBlockModel(List<int[]> meshes) {
+        if (meshes.size() != 7) {
+            throw new IllegalArgumentException();
+        }
+        this.meshes = meshes;
+    }
+
+    @Override
+    public void generate(ClientBlock block, BlockAccessor world, BlockPos pos, BufferBuilder buffer) {
+        buffer.posOffset(pos.getX(), pos.getY(), pos.getZ());
+        BlockPos.Mutable mutablePos = new BlockPos.Mutable(pos);
+        for (Facing facing : Facing.values()) {
+            mutablePos.set(pos);
+            if (!block.canRenderFace(world, mutablePos, facing)) {
+                continue;
+            }
+
+            buffer.put(meshes.get(facing.getIndex()));
+        }
+
+        buffer.put(meshes.get(6));
+    }
+
+    @Override
+    public void generate(ClientBlock block, BufferBuilder buffer) {
+        for (int[] mesh : meshes) {
+            buffer.put(mesh);
+        }
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
---
+++ EngineAPI/src/main/java/unknowndomain/engine/client/rendering/block/model/BlockModelBaker.java
@@ -0,0 +1,8 @@
+package unknowndomain.engine.client.rendering.block.model;
+
+public class BlockModelBaker {
+
+    public BakedBlockModel bake(BlockModel model) {
+        return null;
+    }
+}
