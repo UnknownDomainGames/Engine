886999d97fc45a4ac57786cb4a7c25cd8df694ef
iTNTPiston <44533763+iTNTPiston@users.noreply.github.com>
2019-02-05 00:07:52 -0500

    Done with I18n localize part. Need to load lang files next

--- EngineClient/src/main/java/unknowndomain/engine/client/i18n/I18n.java
+++ EngineClient/src/main/java/unknowndomain/engine/client/i18n/I18n.java
@@ -1,7 +1,7 @@
 package unknowndomain.engine.client.i18n;
 
 public class I18n {
-    public static String localize(String key) {
-        return null;
+    public static String localize(String key, Object... args) {
+        return LocaleManager.getInstance().localize(key, args);
     }
 }
--- EngineClient/src/main/java/unknowndomain/engine/client/i18n/LocaleManager.java
+++ EngineClient/src/main/java/unknowndomain/engine/client/i18n/LocaleManager.java
@@ -1,5 +1,10 @@
 package unknowndomain.engine.client.i18n;
 
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Locale;
+import java.util.Set;
+
 /**
  * keeps all locales
  *
@@ -8,21 +13,51 @@ public class LocaleManager {
 
     private static LocaleManager instance;
 
+    private Hashtable<Locale, Hashtable<String, String>> localeToMap = new Hashtable<>();
+
     private Locale currentLocale = null;
 
     public String localize(String key, Object... args) {
         if (currentLocale == null) {
-
+            setLocale(Locale.getDefault());
         }
-        return null;
+        String unformat = null;
+        // search for locale
+        loadLocale(currentLocale);
+        Hashtable<String, String> table = localeToMap.get(currentLocale);
+        if (table != null) {
+            unformat = table.get(key);
+        }
+
+        if (unformat == null) {
+            return key;
+        }
+
+        return String.format(unformat, args);
     }
 
-    /**
-     * Attempt to init the locale with java.util.Locale
-     */
-    private void initLocale() {
-        java.util.Locale loc = java.util.Locale.getDefault();
-        // TODO
+    public void setLocale(Locale l) {
+        currentLocale = l;
+    }
+
+    public void addMappingFor(Locale l, String key, String value) {
+        loadLocale(l);
+        Hashtable<String, String> table = localeToMap.get(l);
+        table.put(key, value);
+    }
+
+    public void loadLocale(Locale l) {
+        if (localeToMap.containsKey(l))
+            return;
+        Hashtable<String, String> table = new Hashtable<>();
+        // TODO load with AssetsManager
+        if (l.getLanguage().equals("en")) {
+            table.put("test.key", "Test Key");
+        } else {
+            table.put("test.key", "\u6d4b\u8bd5");
+        }
+        localeToMap.put(l, table);
+
     }
 
     public static LocaleManager getInstance() {
