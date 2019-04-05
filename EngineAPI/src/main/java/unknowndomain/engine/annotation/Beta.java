f0ffe6144a54ae4acd636a4d76bed2d14a345c45
Mouse <mouse0w0@qq.com>
2019-04-05 23:02:30 +0800

    Update annotation

--- EngineAPI/src/main/java/unknowndomain/engine/annotation/Beta.java
+++ EngineAPI/src/main/java/unknowndomain/engine/annotation/Beta.java
@@ -1,7 +1,5 @@
 package unknowndomain.engine.annotation;
 
-import unknowndomain.engine.util.Side;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,7 +7,5 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.CONSTRUCTOR, ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
-public @interface SideOnly {
-
-    Side value();
+public @interface Beta {
 }
