fd0a859eedd479586487dc2d9b181e1b55f73217
cvrunmin <cvrunmin@outlook.com>
2019-04-15 19:34:09 +0800

    attempt to fix debug env res path not found in Idea2019.1

--- EngineClient/src/main/java/unknowndomain/engine/client/asset/EngineAssetSource.java
+++ EngineClient/src/main/java/unknowndomain/engine/client/asset/EngineAssetSource.java
@@ -13,7 +13,7 @@ public final class EngineAssetSource extends FileSystemAssetSource {
         try {
             Path path = Paths.get(EngineAssetSource.class.getProtectionDomain().getCodeSource().getLocation().toURI());
             if (Files.isDirectory(path)) {
-                return new EngineAssetSource(FileSystems.getDefault(), path.getParent().resolve("resources").resolve("assets").toString());
+                return new EngineAssetSource(FileSystems.getDefault(), resolveIdeResourcePath(path).toString());
             } else {
                 FileSystem fileSystem = FileSystems.newFileSystem(path, EngineAssetSource.class.getClassLoader());
                 return new EngineAssetSource(fileSystem, "assets");
@@ -21,6 +21,21 @@ public final class EngineAssetSource extends FileSystemAssetSource {
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e); // TODO: log it
         }
+    }
+
+    private static Path resolveIdeResourcePath(Path path) {
+        Path tmp = path;
+        Path gradleSrc = Path.of("");
+        while(tmp.getParent() != null){
+            tmp = tmp.getParent();
+            if(Objects.equals(tmp.getFileName().toString(), "java")){
+                gradleSrc = tmp.relativize(path);
+            }
+            if(Files.exists(tmp.resolveSibling("resources"))){
+                return tmp.resolveSibling("resources").resolve(gradleSrc).resolve("assets");
+            }
+        }
+        return path.getParent().resolve("resources").resolve(gradleSrc).resolve("assets"); //return default value
     }
 
     private EngineAssetSource(FileSystem fileSystem, String root) {
