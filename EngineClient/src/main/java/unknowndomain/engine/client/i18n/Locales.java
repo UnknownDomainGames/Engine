5651bc861ce9ba87ec8a20ae458318e2ae4a629b
sky_bai <a1640727878@163.com>
2019-05-12 15:53:11 +0800

    Update I18n

---
+++ EngineClient/src/main/java/unknowndomain/engine/client/i18n/Locales.java
@@ -0,0 +1,10 @@
+package unknowndomain.engine.client.i18n;
+
+import java.util.Locale;
+
+public class Locales {
+	public static final Locale en_us = new Locale("en","us");
+	public static final Locale zh_cn = new Locale("zh","cn");
+	public static final Locale zh_tw = new Locale("zh","tw");
+	public static final Locale ja_jp = new Locale("ja","jp");
+}
