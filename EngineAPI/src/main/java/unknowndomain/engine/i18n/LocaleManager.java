bdb58296a8b0c67c0200d9f7fd8b14c9fb7be158
Mouse <mouse0w0@qq.com>
2019-06-14 16:11:12 +0800

    format: rename a parameter name

--- EngineAPI/src/main/java/unknowndomain/engine/i18n/LocaleManager.java
+++ EngineAPI/src/main/java/unknowndomain/engine/i18n/LocaleManager.java
@@ -3,7 +3,9 @@ package unknowndomain.engine.i18n;
 import unknowndomain.engine.Platform;
 import unknowndomain.engine.mod.ModContainer;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.HashMap;
@@ -40,8 +42,8 @@ public class LocaleManager {
 		return localeMap.get(locale).get(key);
 	}
 
-	public void setLocale(Locale l) {
-		currentLocale = l;
+    public void setLocale(Locale locale) {
+        currentLocale = locale;
 	}
 
 	public Locale getLocale() {
