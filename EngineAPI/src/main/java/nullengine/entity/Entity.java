7c59d3b2ba800efeff39d511cdef257575f7a57c
Mouse <mouse0w0@qq.com>
2019-07-05 16:49:08 +0800

    docs: add some Entity to-dos

--- EngineAPI/src/main/java/nullengine/entity/Entity.java
+++ EngineAPI/src/main/java/nullengine/entity/Entity.java
@@ -12,11 +12,13 @@ public interface Entity extends GameObject, Tickable {
 
     World getWorld();
 
+    // TODO: Don't use Vector3d, it isn't observable
     Vector3d getPosition();
 
+    // TODO: Don't use Vector3f, it isn't observable
     Vector3f getRotation();
 
-    @Deprecated
+    // TODO: Don't use Vector3f, it isn't observable
     Vector3f getMotion();
 
     AABBd getBoundingBox();
