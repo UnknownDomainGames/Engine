8de7fc056987ccc1051090a800a8003f4ef2754f
Mouse <mouse0w0@qq.com>
2019-07-28 00:53:46 +0800

    feat: storage log and set log level

--- Engine/src/main/java/nullengine/util/TimeUtils.java
+++ Engine/src/main/java/nullengine/util/TimeUtils.java
@@ -7,16 +7,7 @@ import static java.time.temporal.ChronoField.*;
 
 public class TimeUtils {
 
-    public static final DateTimeFormatter FILE_SAFE_DATE_TIME =
-            new DateTimeFormatterBuilder()
-                    .append(DateTimeFormatter.ISO_LOCAL_DATE)
-                    .appendLiteral("_")
-                    .appendValue(HOUR_OF_DAY, 2)
-                    .appendLiteral('.')
-                    .appendValue(MINUTE_OF_HOUR, 2)
-                    .appendLiteral('.')
-                    .appendValue(SECOND_OF_MINUTE, 2)
-                    .toFormatter();
+    public static final DateTimeFormatter FILE_SAFE_DATE_TIME = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
 
     public static final DateTimeFormatter DATE_TIME =
             new DateTimeFormatterBuilder()
