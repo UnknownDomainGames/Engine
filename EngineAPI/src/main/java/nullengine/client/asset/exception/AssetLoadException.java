9cd81fbb327d7f433e8ada30d874ac9e39d50ca1
Mouse <mouse0w0@qq.com>
2019-08-06 19:46:03 +0800

    style: remove unuse constructor

--- EngineAPI/src/main/java/nullengine/client/asset/exception/AssetLoadException.java
+++ EngineAPI/src/main/java/nullengine/client/asset/exception/AssetLoadException.java
@@ -9,8 +9,4 @@ public class AssetLoadException extends RuntimeException {
     public AssetLoadException(String message, Throwable cause) {
         super(message, cause);
     }
-
-    public AssetLoadException(Throwable cause) {
-        super(cause);
-    }
 }
