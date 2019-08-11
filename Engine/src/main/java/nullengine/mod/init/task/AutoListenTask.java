8e74d88adb73a8fa13820226a38fe29d7894b249
Mouse <mouse0w0@qq.com>
2019-08-11 20:22:49 +0800

    feat: change AutoListen value from EventBus array to EventBus

--- Engine/src/main/java/nullengine/mod/init/task/AutoListenTask.java
+++ Engine/src/main/java/nullengine/mod/init/task/AutoListenTask.java
@@ -1,55 +1,53 @@
 package nullengine.mod.init.task;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import nullengine.mod.ModContainer;
 import nullengine.mod.annotation.AutoListen;
 import nullengine.mod.init.ModInitializer;
 import nullengine.util.JsonUtils;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.Optional;
 
 public class AutoListenTask implements ModInitializationTask {
     @Override
     public void run(ModInitializer initializer, ModContainer mod) {
+        JsonArray listeners;
+
         try {
-            Optional<InputStream> stream = mod.getAssets().openStream("META-INF", "data", "AutoListen.json");
+            var stream = mod.getAssets().openStream("META-INF", "data", "AutoListen.json");
             if (stream.isEmpty()) {
                 mod.getLogger().debug("Not found \"AutoListen.json\" file, skip AutoListen stage.");
                 return;
             }
 
-            try (Reader reader = new InputStreamReader(stream.get())) {
-                JsonArray listeners = JsonUtils.DEFAULT_JSON_PARSER.parse(reader).getAsJsonArray();
-                for (JsonElement listener : listeners) {
-                    try {
-                        Class<?> listenerClass = Class.forName(listener.getAsString(), false, mod.getClassLoader());
-                        AutoListen anno = listenerClass.getAnnotation(AutoListen.class);
-                        Object instance = listenerClass.getDeclaredConstructor().newInstance();
-                        for (AutoListen.EventBus bus : anno.value()) {
-                            switch (bus) {
-                                case ENGINE:
-                                default:
-                                    initializer.getEngine().getEventBus().register(instance);
-                                    break;
-                                case MOD:
-                                    mod.getEventBus().register(instance);
-                                    break;
-                            }
-                        }
-                    } catch (Exception e) {
-                        mod.getLogger().warn(String.format("Cannot register listener %s.", listener.getAsString()), e);
-                    }
-                }
+
+            try (var reader = new InputStreamReader(stream.get())) {
+                listeners = JsonUtils.DEFAULT_JSON_PARSER.parse(reader).getAsJsonArray();
             }
+
         } catch (IOException e) {
             mod.getLogger().warn("Cannot open \"AutoListen.json\" file, skip AutoListen stage.", e);
-        } catch (Exception e) {
-            mod.getLogger().warn("Caught exception when auto register listener, skip AutoListen stage.", e);
+            return;
+        }
+
+        for (var listener : listeners) {
+            try {
+                var listenerClass = Class.forName(listener.getAsString(), true, mod.getClassLoader());
+                var anno = listenerClass.getAnnotation(AutoListen.class);
+                var instance = listenerClass.getDeclaredConstructor().newInstance();
+                switch (anno.value()) {
+                    case ENGINE:
+                    default:
+                        initializer.getEngine().getEventBus().register(instance);
+                        break;
+                    case MOD:
+                        mod.getEventBus().register(instance);
+                        break;
+                }
+            } catch (ReflectiveOperationException e) {
+                mod.getLogger().warn(String.format("Cannot register listener %s.", listener.getAsString()), e);
+            }
         }
     }
 }
