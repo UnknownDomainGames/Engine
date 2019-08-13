e959957df6127e5cfef92849f05f45b470b3f36a
Mouse <mouse0w0@qq.com>
2019-08-13 17:43:21 +0800

    feat: support documented for annotations

--- EngineAPI/src/main/java/nullengine/annotation/Beta.java
+++ EngineAPI/src/main/java/nullengine/annotation/Beta.java
@@ -1,11 +1,9 @@
 package nullengine.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.CONSTRUCTOR, ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+@Documented
 public @interface Beta {
 }
