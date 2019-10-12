9956a4875b4189b2d44bb9eae6a954f0a11b8ba8
cvrunmin <cvrunmin@outlook.com>
2019-10-12 20:09:36 +0800

    feat: multi-line text

--- EngineAPI/src/main/java/nullengine/client/gui/rendering/TextRenderer.java
+++ EngineAPI/src/main/java/nullengine/client/gui/rendering/TextRenderer.java
@@ -1,25 +1,46 @@
 package nullengine.client.gui.rendering;
 
-import nullengine.client.gui.Component;
 import nullengine.client.gui.misc.Pos;
 import nullengine.client.gui.text.Text;
 import nullengine.client.rendering.RenderManager;
+import nullengine.client.rendering.font.FontHelper;
 
-public class TextRenderer implements ComponentRenderer {
+import java.util.stream.Collectors;
+
+public class TextRenderer implements ComponentRenderer<Text> {
 
     public static final TextRenderer INSTANCE = new TextRenderer();
 
     @Override
-    public void render(Component component, Graphics graphics, RenderManager context) {
-        Text text = (Text) component;
+    public void render(Text text, Graphics graphics, RenderManager context) {
         graphics.setColor(text.color().getValue());
         graphics.setFont(text.font().getValue());
 
         Pos alignment = text.textAlignment().getValue();
 
         float x = 0, y = 0;
+        var lines = text.text().getValue().lines();
+        var lineHeight = FontHelper.instance().computeTextHeight(text.text().getValue(), text.font().getValue());
+        for (String line : lines.collect(Collectors.toList())) {
+            var lineWidth = FontHelper.instance().computeTextWidth(line, text.font().getValue());
+            switch (alignment.getHpos()) {
+                case RIGHT:
+                    x = text.width().get() - lineWidth;
+                    break;
+                case CENTER:
+                    x = (text.width().get() - lineWidth) / 2;
+                    break;
+                case LEFT:
+                    break;
+            }
+            var y1 = y + (lineHeight * text.leading().getFloat() - lineHeight) / 2;
 
-        switch (alignment.getHpos()) {
+            graphics.pushClipRect(x, y1, text.width().get(), text.height().get());
+            graphics.drawText(line, x, y1);
+            graphics.popClipRect();
+            y += lineHeight * text.leading().getFloat();
+        }
+/*        switch (alignment.getHpos()) {
             case RIGHT:
                 x = text.width().get() - text.prefWidth();
                 break;
@@ -28,9 +49,9 @@ public class TextRenderer implements ComponentRenderer {
                 break;
             case LEFT:
                 break;
-        }
+        }*/
 
-        switch (alignment.getVpos()) {
+/*        switch (alignment.getVpos()) {
             case BOTTOM:
                 y = text.height().get() - text.prefHeight();
                 break;
@@ -39,8 +60,7 @@ public class TextRenderer implements ComponentRenderer {
                 break;
             case TOP:
                 break;
-        }
+        }*/
 
-        graphics.drawText(text.text().getValue(), x, y);
     }
 }
--- EngineAPI/src/main/java/nullengine/client/gui/text/Text.java
+++ EngineAPI/src/main/java/nullengine/client/gui/text/Text.java
@@ -1,6 +1,8 @@
 package nullengine.client.gui.text;
 
+import com.github.mouse0w0.observable.value.MutableDoubleValue;
 import com.github.mouse0w0.observable.value.MutableValue;
+import com.github.mouse0w0.observable.value.SimpleMutableDoubleValue;
 import com.github.mouse0w0.observable.value.SimpleMutableObjectValue;
 import nullengine.client.gui.Component;
 import nullengine.client.gui.misc.Pos;
@@ -10,17 +12,21 @@ import nullengine.client.rendering.font.Font;
 import nullengine.client.rendering.font.FontHelper;
 import nullengine.client.rendering.util.Color;
 
+import java.util.stream.Collectors;
+
 public class Text extends Component {
 
     private final MutableValue<String> text = new SimpleMutableObjectValue<>();
     private final MutableValue<Font> font = new SimpleMutableObjectValue<>(Font.getDefaultFont());
     private final MutableValue<Color> color = new SimpleMutableObjectValue<>(Color.WHITE);
     private final MutableValue<Pos> textAlignment = new SimpleMutableObjectValue<>(Pos.CENTER);
+    private final MutableDoubleValue leading = new SimpleMutableDoubleValue(1.0);
 
     public Text() {
         text.addChangeListener((observable, oldValue, newValue) -> requestParentLayout());
         font.addChangeListener((observable, oldValue, newValue) -> requestParentLayout());
         textAlignment.addChangeListener((observable, oldValue, newValue) -> requestParentLayout());
+        leading.addChangeListener((observable, oldValue, newValue) -> requestParentLayout());
     }
 
     public Text(String text) {
@@ -44,15 +50,24 @@ public class Text extends Component {
         return textAlignment;
     }
 
+    public MutableDoubleValue leading() {
+        return leading;
+    }
+
     @Override
     public float prefWidth() {
-        return (int) FontHelper.instance().computeTextWidth(text().getValue(), font().getValue());
+        var max = -1.0f;
+        for (String s : text.getValue().lines().collect(Collectors.toList())) {
+            max = Math.max(max, FontHelper.instance().computeTextWidth(s, font().getValue()));
+        }
+        return (int) max;
     }
 
     @Override
     public float prefHeight() {
 //        return font().getValue().getSize();
-        return FontHelper.instance().computeTextHeight(text().getValue(), font().getValue());
+        var lines = text().getValue().split("\n").length;
+        return FontHelper.instance().computeTextHeight(text().getValue(), font().getValue()) * leading.getFloat() * lines;
     }
 
     @Override
