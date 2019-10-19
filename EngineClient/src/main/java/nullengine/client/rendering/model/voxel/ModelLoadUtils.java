e4a697e18596dff8c17620f41ed153db1cf9dd98
Mouse <mouse0w0@qq.com>
2019-10-19 14:06:17 +0800

    feat: enchant Transform

--- EngineClient/src/main/java/nullengine/client/rendering/model/voxel/ModelLoadUtils.java
+++ EngineClient/src/main/java/nullengine/client/rendering/model/voxel/ModelLoadUtils.java
@@ -7,7 +7,7 @@ public class ModelLoadUtils {
     public static void fillTransformationArray(Transform[] transforms) {
         for (int i = 0; i < transforms.length; i++) {
             if (transforms[i] == null) {
-                transforms[i] = Transform.DEFAULT;
+                transforms[i] = Transform.IDENTITY;
             }
         }
     }
--- Rendering/src/main/java/nullengine/client/rendering/math/Transform.java
+++ Rendering/src/main/java/nullengine/client/rendering/math/Transform.java
@@ -4,11 +4,15 @@ import org.joml.*;
 
 public class Transform {
 
-    public static final Transform DEFAULT = new Transform(Vectors.VEC3F_ZERO, Vectors.VEC3F_ZERO, Vectors.VEC3F_ONE);
+    public static final Transform IDENTITY = new Transform();
 
     private final Vector3f translation = new Vector3f();
     private final Quaternionf rotation = new Quaternionf();
     private final Vector3f scale = new Vector3f();
+
+    public Transform() {
+        identity();
+    }
 
     public Transform(Vector3fc translation, Vector3fc rotation, Vector3fc scale) {
         this.translation.set(translation);
@@ -26,12 +30,51 @@ public class Transform {
         return translation;
     }
 
+    public void setTranslation(Vector3fc translation) {
+        this.translation.set(translation);
+    }
+
+    public void setTranslation(float x, float y, float z) {
+        this.translation.set(x, y, z);
+    }
+
     public Quaternionf getRotation() {
         return rotation;
     }
 
+    public void setRotation(Vector3fc rotation) {
+        this.rotation.rotateXYZ(rotation.x(), rotation.y(), rotation.z());
+    }
+
+    public void setRotation(float angleX, float angleY, float angleZ) {
+        this.rotation.rotateXYZ(angleX, angleY, angleZ);
+    }
+
+    public void setRotation(Quaternionfc rotation) {
+        this.rotation.set(rotation);
+    }
+
+    public void setRotation(float x, float y, float z, float w) {
+        this.rotation.set(x, y, z, w);
+    }
+
     public Vector3f getScale() {
         return scale;
+    }
+
+    public void setScale(Vector3fc scale) {
+        this.scale.set(scale);
+    }
+
+    public void setScale(float x, float y, float z) {
+        scale.set(x, y, z);
+    }
+
+    public Transform set(Transform transform) {
+        translation.set(transform.translation);
+        rotation.set(transform.rotation);
+        scale.set(transform.scale);
+        return this;
     }
 
     public Vector3f transform(Vector3fc vec) {
@@ -42,7 +85,29 @@ public class Transform {
         return rotation.transform(vec.mul(scale, dest), dest).add(translation);
     }
 
+    public void identity() {
+        translation.set(0, 0, 0);
+        rotation.set(0, 0, 0, 1);
+        scale.set(1, 1, 1);
+    }
+
+    public void applyParent(Transform parent) {
+        translation.mul(parent.scale);
+        parent.rotation.transform(translation, translation).add(parent.translation);
+        parent.rotation.mul(rotation, rotation);
+        scale.mul(parent.scale);
+    }
+
     public Matrix4f toTransformMatrix() {
         return new Matrix4f().scale(this.scale).rotate(rotation).translate(this.translation);
+    }
+
+    @Override
+    public String toString() {
+        return "Transform{" +
+                "translation=" + translation +
+                ", rotation=" + rotation +
+                ", scale=" + scale +
+                '}';
     }
 }
