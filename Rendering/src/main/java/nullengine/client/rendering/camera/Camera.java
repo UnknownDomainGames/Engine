2e913a46e154ac5d6a11d52340222bc9b092778c
Mouse <mouse0w0@qq.com>
2019-10-23 23:44:35 +0800

    style: move Camera

--- Rendering/src/main/java/nullengine/client/rendering/camera/Camera.java
+++ Rendering/src/main/java/nullengine/client/rendering/camera/Camera.java
@@ -1,14 +1,9 @@
 package nullengine.client.rendering.camera;
 
-import nullengine.game.Game;
 import org.joml.Matrix4fc;
 import org.joml.Vector3f;
 import org.joml.Vector3fc;
 
-/**
- * The lifecycle of camera is interesting... A camera can be killed after a game die.
- * <p>So im considering bind camera to {@link Game}</p>
- */
 public interface Camera {
 
     Vector3fc UP_VECTOR = new Vector3f(0, 1, 0);
@@ -21,10 +16,5 @@ public interface Camera {
 
     Vector3fc getFrontVector();
 
-    /**
-     * parse view matrix for shader to use
-     *
-     * @return
-     */
     Matrix4fc getViewMatrix();
 }
