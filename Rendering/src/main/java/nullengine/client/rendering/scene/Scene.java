4e329fe5ffac114342641549370978b35f92d018
Mouse <mouse0w0@qq.com>
2019-11-10 23:08:44 +0800

    feat(rendering): update scene

--- Rendering/src/main/java/nullengine/client/rendering/scene/Scene.java
+++ Rendering/src/main/java/nullengine/client/rendering/scene/Scene.java
@@ -1,15 +1,33 @@
 package nullengine.client.rendering.scene;
 
+import com.github.mouse0w0.observable.collection.ObservableCollections;
+import com.github.mouse0w0.observable.collection.ObservableList;
 import nullengine.client.rendering.queue.RenderQueue;
 import nullengine.client.rendering.scene.light.LightManager;
 
-public class Scene extends Node {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scene {
+
+    private final ObservableList<Node> children = ObservableCollections.observableList(new ArrayList<>());
 
     private final RenderQueue renderQueue = new RenderQueue();
     private final LightManager lightManager = new LightManager();
 
     public Scene() {
-        scene.set(this);
+        children.addChangeListener(change -> {
+            for (Node node : change.getAdded()) node.scene.set(Scene.this);
+            for (Node node : change.getRemoved()) node.scene.set(null);
+        });
+    }
+
+    public List<Node> getChildren() {
+        return children;
+    }
+
+    public Node getNode(String id) {
+        return null;
     }
 
     public RenderQueue getRenderQueue() {
@@ -19,4 +37,6 @@ public class Scene extends Node {
     public LightManager getLightManager() {
         return lightManager;
     }
+
+
 }
