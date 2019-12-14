a2532c1cdda704e0fa31b5021a20a41498747def
Mouse <mouse0w0@qq.com>
2019-12-15 01:08:02 +0800

    feat: move Beta and Experimental annotations

---
+++ Common/src/main/java/nullengine/annotation/Beta.java
@@ -0,0 +1,13 @@
+package nullengine.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Annotate any beta code library.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.CONSTRUCTOR, ElementType.TYPE, ElementType.METHOD,
+        ElementType.FIELD, ElementType.PACKAGE, ElementType.MODULE})
+@Documented
+public @interface Beta {
+}
--- EngineAPI/src/main/java/nullengine/annotation/Beta.java
+++
@@ -1,9 +0,0 @@
-package nullengine.annotation;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.CLASS)
-@Target({ElementType.CONSTRUCTOR, ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
-@Documented
-public @interface Beta {
-}
