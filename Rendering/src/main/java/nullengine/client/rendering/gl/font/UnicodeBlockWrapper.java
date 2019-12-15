0cd8cf34dc19679e8610817d20021eb1c9b3c136
Mouse <mouse0w0@qq.com>
2019-12-15 14:01:46 +0800

    feat(font): add logger to UnicodeBlockWrapper

--- Rendering/src/main/java/nullengine/client/rendering/gl/font/UnicodeBlockWrapper.java
+++ Rendering/src/main/java/nullengine/client/rendering/gl/font/UnicodeBlockWrapper.java
@@ -1,18 +1,23 @@
 package nullengine.client.rendering.gl.font;
 
 import com.google.common.collect.Range;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class UnicodeBlockWrapper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UnicodeBlockWrapper.class);
+
     private static final Map<Character.UnicodeBlock, Range<Integer>> map = new HashMap<>();
 
-    public static Range<Integer> getRange(Character.UnicodeBlock block){
+    public static Range<Integer> getRange(Character.UnicodeBlock block) {
         return map.get(block);
     }
 
-    public static int getBlockSize(Character.UnicodeBlock block){
+    public static int getBlockSize(Character.UnicodeBlock block) {
         var range = getRange(block);
         return range.upperEndpoint() - range.lowerEndpoint() + 1;
     }
@@ -27,20 +32,19 @@ public class UnicodeBlockWrapper {
             var blockStarts = (int[]) blksField.get(null);
             // blocks and blockStarts has the same length, and the last block recorded ends with code point 0x10FFFF
             for (int i = 0; i < blocks.length; i++) {
-                if(blocks[i] == null) continue; //unassigned
+                if (blocks[i] == null) continue; //unassigned
                 int rangeStart = blockStarts[i];
                 int rangeEnd;
-                if(i == blocks.length - 1){ //last block recorded
+                if (i == blocks.length - 1) { //last block recorded
                     rangeEnd = 0x10FFFF;
-                }
-                else{
+                } else {
                     rangeEnd = blockStarts[i + 1] - 1;
                 }
                 var range = Range.closed(rangeStart, rangeEnd);
                 map.put(blocks[i], range);
             }
         } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
+            LOGGER.error("Cannot initialize unicode block.", e);
         }
     }
 }
