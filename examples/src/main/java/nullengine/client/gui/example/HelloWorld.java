2de49d987ccc24f73831369cbe8039076d4c2e77
Mouse <mouse0w0@qq.com>
2020-01-10 23:38:02 +0800

    feat(examples): add gui example HelloWorld

---
+++ examples/src/main/java/nullengine/client/gui/example/HelloWorld.java
@@ -0,0 +1,24 @@
+package nullengine.client.gui.example;
+
+import nullengine.client.gui.Scene;
+import nullengine.client.gui.application.GUIApplication;
+import nullengine.client.gui.layout.FlowPane;
+import nullengine.client.gui.misc.Pos;
+import nullengine.client.gui.text.Text;
+import nullengine.client.rendering.display.Window;
+
+public class HelloWorld extends GUIApplication {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Window primaryWindow) throws Exception {
+        FlowPane flowPane = new FlowPane();
+        flowPane.alignment().set(Pos.CENTER);
+        Text text = new Text("Hello World");
+        flowPane.getChildren().add(text);
+        Scene scene = new Scene(flowPane);
+        scene.bindWindow(primaryWindow);
+    }
+}
