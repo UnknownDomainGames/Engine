1c8994c664c37eb8477d27454dbc32e45a31b423
cvrunmin <cvrunmin@outlook.com>
2020-01-11 21:03:49 +0800

    feat: shader info collector (wip)

---
+++ rendering/src/main/java/nullengine/client/rendering/gl/shader/ShaderFormatException.java
@@ -0,0 +1,23 @@
+package nullengine.client.rendering.gl.shader;
+
+public class ShaderFormatException extends RuntimeException {
+    public ShaderFormatException() {
+        super();
+    }
+
+    public ShaderFormatException(String message) {
+        super(message);
+    }
+
+    public ShaderFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ShaderFormatException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ShaderFormatException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
