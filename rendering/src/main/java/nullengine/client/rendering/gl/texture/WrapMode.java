0ae1cb77a9b862f42ed0bed404a02a4df20b502f
Mouse <mouse0w0@qq.com>
2020-01-22 23:57:55 +0800

    feat(rendering): update WrapMode

--- rendering/src/main/java/nullengine/client/rendering/gl/texture/WrapMode.java
+++ rendering/src/main/java/nullengine/client/rendering/gl/texture/WrapMode.java
@@ -3,11 +3,15 @@ package nullengine.client.rendering.gl.texture;
 import static org.lwjgl.opengl.GL11.GL_CLAMP;
 import static org.lwjgl.opengl.GL11.GL_REPEAT;
 import static org.lwjgl.opengl.GL12.GL_CLAMP_TO_EDGE;
+import static org.lwjgl.opengl.GL13.GL_CLAMP_TO_BORDER;
+import static org.lwjgl.opengl.GL14.GL_MIRRORED_REPEAT;
 
 public enum WrapMode {
     CLAMP(GL_CLAMP),
     REPEAT(GL_REPEAT),
-    CLAMP_TO_EDGE(GL_CLAMP_TO_EDGE);
+    CLAMP_TO_EDGE(GL_CLAMP_TO_EDGE),
+    CLAMP_TO_BORDER(GL_CLAMP_TO_BORDER),
+    MIRRORED_REPEAT(GL_MIRRORED_REPEAT);
 
     public final int gl;
 
