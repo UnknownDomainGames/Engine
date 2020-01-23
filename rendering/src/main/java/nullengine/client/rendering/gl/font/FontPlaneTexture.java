5b6078adc76eb6d30855aebbb541f6d8f70d647a
Mouse <mouse0w0@qq.com>
2020-01-23 17:51:31 +0800

    fix(rendering): fix an error of FontPlaneTexture

--- rendering/src/main/java/nullengine/client/rendering/gl/font/FontPlaneTexture.java
+++ rendering/src/main/java/nullengine/client/rendering/gl/font/FontPlaneTexture.java
@@ -2,12 +2,11 @@ package nullengine.client.rendering.gl.font;
 
 import nullengine.client.rendering.font.Font;
 import nullengine.client.rendering.font.UnicodeBlockWrapper;
-import nullengine.client.rendering.gl.texture.FilterMode;
 import nullengine.client.rendering.gl.texture.GLTexture2D;
+import nullengine.client.rendering.texture.FilterMode;
 import nullengine.math.Math2;
 import org.joml.Vector4f;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
 import org.lwjgl.stb.STBTTAlignedQuad;
 import org.lwjgl.stb.STBTTPackContext;
 import org.lwjgl.stb.STBTTPackRange;
@@ -19,11 +18,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_RED;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_BYTE;
 import static org.lwjgl.stb.STBTruetype.*;
 
 public class FontPlaneTexture {
-//    private List<Integer> abandonedTexIds;
+    //    private List<Integer> abandonedTexIds;
     private GLTexture2D glTexture;
     private Font font;
     private int bitmapWidth;
@@ -34,43 +34,43 @@ public class FontPlaneTexture {
     private Map<Character, CharQuad> charQuads;
     private boolean dirty = false;
 
-    public FontPlaneTexture(){
+    public FontPlaneTexture() {
 //        abandonedTexIds = new ArrayList<>();
         blocks = new ArrayList<>();
         charQuads = new HashMap<>();
     }
 
-    public void bind(){
-        if(glTexture != null){
+    public void bind() {
+        if (glTexture != null) {
             glTexture.bind();
         }
     }
 
-    public void putBlock(Character.UnicodeBlock block){
-        if(!blocks.contains(block)) { //Make sure we don't load the same block twice
+    public void putBlock(Character.UnicodeBlock block) {
+        if (!blocks.contains(block)) { //Make sure we don't load the same block twice
             blocks.add(block);
             dirty = true;
         }
     }
 
-    public void removeBlock(Character.UnicodeBlock block){
+    public void removeBlock(Character.UnicodeBlock block) {
         blocks.remove(block);
     }
 
-    public boolean isBlockInList(Character.UnicodeBlock block){
+    public boolean isBlockInList(Character.UnicodeBlock block) {
         return blocks.contains(block);
     }
 
-    public boolean isWaitingForReloading(){
+    public boolean isWaitingForReloading() {
         return dirty;
     }
 
-    public CharQuad getQuad(char ch){
+    public CharQuad getQuad(char ch) {
         return charQuads.get(ch);
     }
 
-    public void bakeTexture(Font font, NativeTTFontInfo fontInfo){
-        if(glTexture != null){
+    public void bakeTexture(Font font, NativeTTFontInfo fontInfo) {
+        if (glTexture != null) {
             glTexture.dispose();
         }
         this.font = font;
@@ -81,21 +81,21 @@ public class FontPlaneTexture {
         bitmapWidth = bitmapSize;
         bitmapHeight = bitmapSize;
         ByteBuffer bitmap = ByteBuffer.allocateDirect(bitmapSize * bitmapSize);
-        try(var context = STBTTPackContext.malloc()) {
+        try (var context = STBTTPackContext.malloc()) {
             var ranges = STBTTPackRange.malloc(blocks.size());
             for (Character.UnicodeBlock block : blocks) {
                 var range = STBTTPackRange.malloc();
                 var blockSize1 = UnicodeBlockWrapper.getBlockSize(block);
                 var charBuffer = STBTTPackedchar.malloc(blockSize1);
-                range.set(font.getSize(), UnicodeBlockWrapper.getRange(block).lowerEndpoint(), null, blockSize1, charBuffer,(byte)1,(byte)1);
+                range.set(font.getSize(), UnicodeBlockWrapper.getRange(block).lowerEndpoint(), null, blockSize1, charBuffer, (byte) 1, (byte) 1);
                 ranges.put(range);
             }
             ranges.flip();
             stbtt_PackBegin(context, bitmap, bitmapSize, bitmapSize, 0, 1);
-            stbtt_PackFontRanges(context, fontInfo.getFontData(),fontInfo.getOffsetIndex(), ranges);
+            stbtt_PackFontRanges(context, fontInfo.getFontData(), fontInfo.getOffsetIndex(), ranges);
             stbtt_PackEnd(context);
 
-            glTexture = GLTexture2D.builder().format(GL_RED).internalFormat(GL_RED).type(GL_UNSIGNED_BYTE).magFilter(FilterMode.LINEAR).minFilter(FilterMode.LINEAR).level(0).build(bitmap, bitmapSize,bitmapSize);
+            glTexture = GLTexture2D.builder().format(GL_RED).internalFormat(GL_RED).type(GL_UNSIGNED_BYTE).magFilter(FilterMode.LINEAR).minFilter(FilterMode.LINEAR).build(bitmap, bitmapSize, bitmapSize);
             STBTTAlignedQuad stbQuad = STBTTAlignedQuad.mallocStack();
             for (int i = 0; i < blocks.size(); i++) {
                 FloatBuffer posX = BufferUtils.createFloatBuffer(1);
@@ -104,11 +104,11 @@ public class FontPlaneTexture {
                 var startPoint = UnicodeBlockWrapper.getRange(blocks.get(i)).lowerEndpoint();
                 var cdata = ranges.get(i).chardata_for_range();
                 for (int j = 0; j < UnicodeBlockWrapper.getBlockSize(blocks.get(i)); j++) {
-                    stbtt_GetPackedQuad(cdata,bitmapWidth,bitmapHeight, j, posX, posY, stbQuad, false);
-                    var quads = new Vector4f(stbQuad.x0(),stbQuad.y0(), stbQuad.x1(), stbQuad.y1());
-                    var tex = new Vector4f(stbQuad.s0(),stbQuad.t0(), stbQuad.s1(), stbQuad.t1());
+                    stbtt_GetPackedQuad(cdata, bitmapWidth, bitmapHeight, j, posX, posY, stbQuad, false);
+                    var quads = new Vector4f(stbQuad.x0(), stbQuad.y0(), stbQuad.x1(), stbQuad.y1());
+                    var tex = new Vector4f(stbQuad.s0(), stbQuad.t0(), stbQuad.s1(), stbQuad.t1());
                     charQuads.put((char) (startPoint + j), new CharQuad((char) (startPoint + j), quads, tex, posX.get(0)));
-                    posX.put(0,0);
+                    posX.put(0, 0);
                     posY.put(0, font.getSize());
                 }
             }
@@ -120,13 +120,13 @@ public class FontPlaneTexture {
         return Math2.ceilPowerOfTwo((int) Math.ceil(size * Math.sqrt(countOfChar)));
     }
 
-    public static class CharQuad{
+    public static class CharQuad {
         private final Vector4f pos;
         private final Vector4f texCoord;
         private final char character;
         private final float xOffset;
 
-        public CharQuad(char character, Vector4f pos, Vector4f texCoord, float xOffset){
+        public CharQuad(char character, Vector4f pos, Vector4f texCoord, float xOffset) {
             this.character = character;
             this.pos = pos;
             this.texCoord = texCoord;
