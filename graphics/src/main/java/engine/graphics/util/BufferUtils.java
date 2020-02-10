4f6cc37a2e3b8abf366b88eec98a996a182f02d4
Mouse <mouse0w0@qq.com>
2020-02-11 01:04:48 +0800

    feat(graphics): add BufferUtils

---
+++ graphics/src/main/java/engine/graphics/util/BufferUtils.java
@@ -0,0 +1,47 @@
+package engine.graphics.util;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public final class BufferUtils {
+    private BufferUtils() {
+    }
+
+    public static ByteBuffer createByteBuffer(int capacity) {
+        return ByteBuffer.allocateDirect(capacity).order(ByteOrder.nativeOrder());
+    }
+
+    public static ByteBuffer wrapAsByteBuffer(byte[] src) {
+        return createByteBuffer(src.length).put(src).flip();
+    }
+
+    public static ByteBuffer wrapAsByteBuffer(short[] src) {
+        ByteBuffer byteBuffer = createByteBuffer(src.length << 1);
+        byteBuffer.asShortBuffer().put(src).flip();
+        return byteBuffer;
+    }
+
+    public static ByteBuffer wrapAsByteBuffer(int[] src) {
+        ByteBuffer byteBuffer = createByteBuffer(src.length << 2);
+        byteBuffer.asIntBuffer().put(src).flip();
+        return byteBuffer;
+    }
+
+    public static ByteBuffer wrapAsByteBuffer(long[] src) {
+        ByteBuffer byteBuffer = createByteBuffer(src.length << 3);
+        byteBuffer.asLongBuffer().put(src).flip();
+        return byteBuffer;
+    }
+
+    public static ByteBuffer wrapAsByteBuffer(float[] src) {
+        ByteBuffer byteBuffer = createByteBuffer(src.length << 2);
+        byteBuffer.asFloatBuffer().put(src).flip();
+        return byteBuffer;
+    }
+
+    public static ByteBuffer wrapAsByteBuffer(double[] src) {
+        ByteBuffer byteBuffer = createByteBuffer(src.length << 3);
+        byteBuffer.asDoubleBuffer().put(src).flip();
+        return byteBuffer;
+    }
+}
