8eb277dfbba2047a6a8a6a306c67ceafb1743010
Mouse <mouse0w0@qq.com>
2020-02-27 22:00:42 +0800

    style(graphics3d): clean code

--- graphics3d/src/main/java/engine/graphics/queue/RenderQueue.java
+++ graphics3d/src/main/java/engine/graphics/queue/RenderQueue.java
@@ -1,7 +1,6 @@
 package engine.graphics.queue;
 
 import engine.graphics.Geometry;
-import engine.graphics.management.GraphicsBackend;
 
 import javax.annotation.Nonnull;
 import java.util.HashMap;
@@ -10,6 +9,10 @@ import java.util.Map;
 public class RenderQueue {
 
     private final Map<RenderType, GeometryList> queue = new HashMap<>();
+
+    public GeometryList getGeometryList(RenderType type) {
+        return queue.get(type);
+    }
 
     public void add(@Nonnull Geometry geometry, RenderType layer) {
         if (layer == null) return;
@@ -21,17 +24,7 @@ public class RenderQueue {
         queue.computeIfAbsent(layer, key -> new GeometryList()).remove(geometry);
     }
 
-    public GeometryList getGeometryList(RenderType type) {
-        return queue.get(type);
-    }
-
     public void clear() {
         queue.clear();
-    }
-
-    public void render(GraphicsBackend manager, RenderType layer, RenderTypeHandler handler) {
-        GeometryList geometries = queue.get(layer);
-        if (geometries == null) return;
-        handler.render(manager, geometries);
     }
 }
