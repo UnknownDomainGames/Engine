32c956c058a7d7041d26dbe19aec019b80432ec7
Mouse <mouse0w0@qq.com>
2020-03-05 20:13:38 +0800

    fix(gui): fix a compile error

--- gui/src/main/java/engine/gui/control/Control.java
+++ gui/src/main/java/engine/gui/control/Control.java
@@ -4,11 +4,6 @@ import engine.gui.Region;
 
 public abstract class Control extends Region {
 
-    @Override
-    public boolean isResizable() {
-        return true;
-    }
-
 //    @Override
 //    public List<Node> getPointingComponents(float posX, float posY) {
 //        return Lists.newArrayList(this);
