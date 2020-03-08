f3222562a31d9e91e76f90fddd3307455c480d76
Mouse <mouse0w0@qq.com>
2020-03-08 14:56:22 +0800

    fix(gui): fix not set scene size when changed scene

--- gui/src/main/java/engine/gui/stage/Stage.java
+++ gui/src/main/java/engine/gui/stage/Stage.java
@@ -102,6 +102,7 @@ public class Stage {
             }
             if (newValue != null) {
                 SceneHelper.setStage(newValue, Stage.this);
+                SceneHelper.setSize(newValue, getWidth() / getScaleX(), getHeight() / getScaleY());
             }
         });
     }
