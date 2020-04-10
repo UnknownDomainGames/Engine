1c84172e4555b1879acfbff278113282eedcfe3a
Mouse <mouse0w0@qq.com>
2020-04-10 10:33:23 +0800

    feat(common): update eventbus

--- common/src/main/java/engine/event/SimpleEventBus.java
+++ common/src/main/java/engine/event/SimpleEventBus.java
@@ -154,7 +154,7 @@ public class SimpleEventBus implements EventBus {
         if (registeredListeners.containsKey(consumer)) {
             throw new IllegalStateException("Listener has been registered");
         }
-        RegisteredListener listener = new RegisteredListener(eventType, null, order, receiveCancelled, null, event -> consumer.accept(eventType.cast(event)));
+        RegisteredListener listener = new RegisteredListener(eventType, consumer, order, receiveCancelled, null, event -> consumer.accept(eventType.cast(event)));
         registeredListeners.put(listener, List.of(listener));
         getListenerList(eventType).register(listener);
     }
@@ -179,7 +179,7 @@ public class SimpleEventBus implements EventBus {
         if (registeredListeners.containsKey(consumer)) {
             throw new IllegalStateException("Listener has been registered");
         }
-        RegisteredListener listener = new RegisteredListener(eventType, null, order, receiveCancelled, genericType, event -> consumer.accept(eventType.cast(event)));
+        RegisteredListener listener = new RegisteredListener(eventType, consumer, order, receiveCancelled, genericType, event -> consumer.accept(eventType.cast(event)));
         registeredListeners.put(listener, List.of(listener));
         getListenerList(eventType).register(listener);
     }
