6b16fcfdb4f42dabb35ac00845ff391aab8e4468
Mouse <mouse0w0@qq.com>
2020-04-16 20:39:12 +0800

    feat(gui): update EventUtils

--- gui/src/main/java/engine/gui/event/EventUtils.java
+++ gui/src/main/java/engine/gui/event/EventUtils.java
@@ -1,9 +1,25 @@
 package engine.gui.event;
 
-public class EventUtils {
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public final class EventUtils {
+
+    private static final EventDispatchChainImpl EVENT_DISPATCH_CHAIN = new EventDispatchChainImpl();
+    private static final AtomicBoolean EVENT_DISPATCH_CHAIN_IN_USE = new AtomicBoolean();
 
     public static Event fireEvent(Event event, EventTarget eventTarget) {
-        EventDispatchChainImpl eventDispatchChain = new EventDispatchChainImpl();
-        return eventTarget.buildEventDispatchChain(eventDispatchChain).dispatchEvent(event);
+        if (EVENT_DISPATCH_CHAIN_IN_USE.getAndSet(true)) {
+            return eventTarget.buildEventDispatchChain(new EventDispatchChainImpl()).dispatchEvent(event);
+        }
+
+        try {
+            return eventTarget.buildEventDispatchChain(EVENT_DISPATCH_CHAIN).dispatchEvent(event);
+        } finally {
+            EVENT_DISPATCH_CHAIN.reset();
+            EVENT_DISPATCH_CHAIN_IN_USE.set(false);
+        }
+    }
+
+    private EventUtils() {
     }
 }
