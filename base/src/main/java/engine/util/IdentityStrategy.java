586031fb087a89fce5eda8f2f101c521149d6648
Mouse <mouse0w0@qq.com>
2020-04-29 21:19:18 +0800

    feat: add IdentityStrategy

---
+++ base/src/main/java/engine/util/IdentityStrategy.java
@@ -0,0 +1,24 @@
+package engine.util;
+
+import it.unimi.dsi.fastutil.Hash;
+
+public final class IdentityStrategy<K> implements Hash.Strategy<K> {
+    private static final IdentityStrategy<?> INSTANCE = new IdentityStrategy<>();
+
+    public static <K> IdentityStrategy<K> instance() {
+        return (IdentityStrategy<K>) INSTANCE;
+    }
+
+    private IdentityStrategy() {
+    }
+
+    @Override
+    public int hashCode(K o) {
+        return System.identityHashCode(o);
+    }
+
+    @Override
+    public boolean equals(K a, K b) {
+        return a == b;
+    }
+}
