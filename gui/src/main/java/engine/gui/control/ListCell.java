817698b422e56276174b3a3f5a4da8d3353445f1
cvrunmin <cvrunmin@outlook.com>
2020-05-09 16:43:19 +0800

    feat: ListCell

---
+++ gui/src/main/java/engine/gui/control/ListCell.java
@@ -0,0 +1,162 @@
+package engine.gui.control;
+
+import com.github.mouse0w0.observable.collection.ListChangeListener;
+import com.github.mouse0w0.observable.collection.ObservableList;
+import com.github.mouse0w0.observable.value.*;
+
+import java.util.Objects;
+
+public class ListCell<T> extends Labeled {
+
+    private MutableObjectValue<ListView<T>> owner = new SimpleMutableObjectValue<>();
+
+    private MutableObjectValue<T> item = new SimpleMutableObjectValue<>();
+
+    private MutableIntValue index = new SimpleMutableIntValue(-1);
+
+    private MutableBooleanValue markedEmpty = new SimpleMutableBooleanValue();
+
+    private MutableBooleanValue selected = new SimpleMutableBooleanValue();
+
+    protected void updateItem(T item, boolean empty) {
+        this.item.set(item);
+        this.markedEmpty.set(empty);
+        if (empty && selected.get()) {
+            setSelected(false);
+        }
+    }
+
+    protected boolean isItemChanged(T oldItem, T newItem) {
+        return !Objects.equals(oldItem, newItem);
+    }
+
+    public void setSelected(boolean flag) {
+        if (flag && markedEmpty.get()) return;
+        selected.set(flag);
+    }
+
+    public ObservableBooleanValue selected() {
+        return selected.toUnmodifiable();
+    }
+
+    public MutableBooleanValue markedEmpty() {
+        return markedEmpty;
+    }
+
+    public MutableIntValue index() {
+        return index;
+    }
+
+    public MutableObjectValue<T> item() {
+        return item;
+    }
+
+    public MutableObjectValue<ListView<T>> owner() {
+        return owner;
+    }
+
+    private final ListChangeListener<T> itemChangeListener = c -> {
+        var index = this.index.get();
+        var items = owner.isPresent() ? owner.get().items() : null;
+        var itemCount = items == null ? 0 : items.size();
+
+        var indexAfterChangeFromIndex = index >= c.getFrom();
+        var indexBeforeChangeToIndex = index < c.getTo() || index == itemCount;
+        var indexInRange = indexAfterChangeFromIndex && indexBeforeChangeToIndex;
+
+        boolean shouldUpdate = indexInRange || (indexAfterChangeFromIndex && (c.wasRemoved() || c.wasAdded()));
+
+        if (shouldUpdate) {
+            updateItem(-1);
+        }
+    };
+    private final ValueChangeListener<Integer> selectedIndexChangeListener = (observable1, oldValue1, newValue1) -> {
+        updateSelection();
+    };
+    private final ValueChangeListener<ObservableList<T>> itemListChangeListener = (observable1, oldValue1, newValue1) -> {
+        if (oldValue1 != null) {
+            oldValue1.removeChangeListener(itemChangeListener);
+        }
+        if (newValue1 != null) {
+            newValue1.addChangeListener(itemChangeListener);
+        }
+        updateItem(-1);
+    };
+    private final ValueChangeListener<SelectionModel<T>> selectionModelChangeListener = (observable1, oldValue1, newValue1) -> {
+        if (oldValue1 != null) {
+            oldValue1.selectedIndex().removeChangeListener(selectedIndexChangeListener);
+        }
+        if (newValue1 != null) {
+            newValue1.selectedIndex().addChangeListener(selectedIndexChangeListener);
+        }
+        updateSelection();
+    };
+
+    public ListCell() {
+        setText(null);
+        owner.addChangeListener((observable, oldValue, newValue) -> {
+            if (oldValue != null) {
+                oldValue.observableItems().ifPresent(list -> list.removeChangeListener(itemChangeListener));
+                oldValue.selectionModel().ifPresent(model -> model.selectedIndex().removeChangeListener(selectedIndexChangeListener));
+                oldValue.observableItems().removeChangeListener(itemListChangeListener);
+                oldValue.selectionModel().removeChangeListener(selectionModelChangeListener);
+            }
+            if (newValue != null) {
+                newValue.observableItems().ifPresent(list -> list.addChangeListener(itemChangeListener));
+                newValue.selectionModel().ifPresent(model -> model.selectedIndex().addChangeListener(selectedIndexChangeListener));
+                newValue.observableItems().addChangeListener(itemListChangeListener);
+                newValue.selectionModel().addChangeListener(selectionModelChangeListener);
+            }
+            updateItem(-1);
+            updateSelection();
+            needsLayout();
+        });
+        index.addChangeListener((observable, oldValue, newValue) -> {
+            if (newValue != oldValue) {
+                updateItem(oldValue);
+                updateSelection();
+            }
+        });
+    }
+
+    private boolean initializedEmpty;
+
+    private void updateItem(int oldIndex) {
+        var index = index().get();
+        var items = owner.isPresent() ? owner.get().items() : null;
+        var size = items != null ? items.size() : 0;
+
+        var valid = items != null && 0 <= index && index < size;
+        var oldItem = item.get();
+        var empty = markedEmpty.get();
+        if (valid) {
+            var newItem = items.get(index);
+            if (oldIndex != index || isItemChanged(oldItem, newItem)) {
+                updateItem(newItem, false);
+            }
+        } else {
+            if ((!empty && oldItem != null) || !initializedEmpty) {
+                updateItem(null, true);
+                initializedEmpty = true;
+            }
+        }
+    }
+
+    private void updateSelection() {
+        if (markedEmpty.get()) return;
+        var index = index().get();
+        if (index == -1 || owner.isEmpty()) return;
+
+        var model = owner.get().selectionModel().get();
+        if (model == null) {
+            setSelected(false);
+        } else {
+            boolean isSelected = model.isSelected(index);
+            if (selected.get() != isSelected) {
+                setSelected(isSelected);
+            }
+        }
+
+    }
+
+}
--- gui/src/main/java/engine/gui/control/ListView.java
+++ gui/src/main/java/engine/gui/control/ListView.java
@@ -10,16 +10,44 @@ import engine.gui.layout.Pane;
 import engine.gui.layout.VBox;
 import engine.gui.misc.Orientation;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 
 public class ListView<T> extends Control {
 
     private MutableObjectValue<Orientation> orientation;
 
-    private MutableObjectValue<Function<T, ? extends Node>> cellFactory = new SimpleMutableObjectValue<>((value) -> new Text(value.toString()));
+    @SuppressWarnings("Convert2Diamond")
+    // T in ListCell<T> inside lambda has to be explicitly stated in order to compile successfully
+    private MutableObjectValue<Function<ListView<T>, ListCell<T>>> cellFactory = new SimpleMutableObjectValue<>((listView) -> new ListCell<T>() {
+        @Override
+        protected void updateItem(T item, boolean empty) {
+            super.updateItem(item, empty);
 
-    private ObservableList<T> items = ObservableCollections.observableList(new ArrayList<>());
+            if (empty) {
+                setGraphic(null);
+                setText(null);
+            } else if (item instanceof Node) {
+                setText(null);
+                var oldNode = getGraphic();
+                var newNode = (Node) item;
+                if (oldNode == null || !oldNode.equals(newNode)) {
+                    setGraphic(newNode);
+                }
+            } else {
+                setText(item == null ? "null" : item.toString());
+                setGraphic(null);
+            }
+        }
+    });
+
+    private MutableObjectValue<ObservableList<T>> items = new SimpleMutableObjectValue<>(ObservableCollections.observableList(new ArrayList<>()));
+
+    private MutableObjectValue<SelectionModel<T>> selectionModel = new SimpleMutableObjectValue<>(new SingleSelectionModel<>(items()));
 
     private ScrollPane scrollPane;
     private Pane contentPane;
@@ -29,12 +57,7 @@ public class ListView<T> extends Control {
         getChildren().add(scrollPane);
         this.getSize().prefWidth().bindBidirectional(scrollPane.getSize().prefWidth());
         this.getSize().prefHeight().bindBidirectional(scrollPane.getSize().prefHeight());
-        items.addChangeListener(change -> {
-            // TODO: waiting for optimization
-            var contentChildren = contentPane.getChildren();
-            var cellFactory = this.cellFactory.get();
-            contentChildren.clear();
-            for (T item : items) contentChildren.add(cellFactory.apply(item));
+        items.get().addChangeListener(change -> {
             needsLayout();
         });
 //        padding().addChangeListener((observable, oldValue, newValue) -> layoutInArea(scrollPane));
@@ -82,7 +105,101 @@ public class ListView<T> extends Control {
         return scrollPane.computeHeight();
     }
 
-    public ObservableList<T> items() {
+    public MutableObjectValue<ObservableList<T>> observableItems() {
         return items;
+    }
+
+    public ObservableList<T> items() {
+        return items.get();
+    }
+
+    public MutableObjectValue<SelectionModel<T>> selectionModel() {
+        return selectionModel;
+    }
+
+    private LinkedList<ListCell<T>> cells = new LinkedList<>();
+    private LinkedList<ListCell<T>> pile = new LinkedList<>();
+
+    protected void dumpToPile() {
+        var size = cells.size();
+        for (int i = 0; i < size; i++) {
+            addToPile(cells.removeFirst());
+        }
+    }
+
+    protected void addToPile(ListCell<T> cell) {
+        Objects.requireNonNull(cell);
+
+        pile.addLast(cell);
+    }
+
+    @Nonnull
+    private ListCell<T> getAvailableCell(int prefIndex) {
+        ListCell<T> cell = null;
+
+        var size = pile.size();
+        for (int i = 0; i < size; i++) {
+            var _cell = pile.get(i);
+            if (_cell == null) continue;
+
+            if (_cell.index().get() == prefIndex) {
+                cell = _cell;
+                pile.remove(i);
+                break;
+            }
+        }
+
+        if (cell == null) {
+            if (!pile.isEmpty()) {
+                //TODO: if we need even-odd styling, pick one from pile
+                cell = pile.removeFirst();
+            } else {
+                var cellFactory = this.cellFactory.get();
+                cell = cellFactory.apply(this);
+            }
+        }
+
+        return cell;
+    }
+
+    int lastCellCount = -1;
+
+    @Override
+    protected void layoutChildren() {
+        // TODO: waiting for optimization
+        var contentChildren = contentPane.getChildren();
+        contentChildren.clear();
+        boolean needRecreateCells = false; //TODO: field
+        if (needRecreateCells) {
+            for (var cell : cells) {
+                cell.index().set(-1);
+                cell.owner().set(null);
+            }
+            cells.clear();
+        }
+        boolean layoutCellsOnly = false;
+        if (layoutCellsOnly) {
+            for (ListCell<T> cell : cells) {
+                cell.needsLayout();
+            }
+            layoutCellsOnly = false;
+        } else {
+
+            var shouldRebuild = cells.isEmpty() || lastCellCount != observableItems().optional().map(ObservableList::size).orElse(0);
+            if (shouldRebuild) {
+                //TODO: we might store old cells into a pile, and then retrieve it back here if suitable
+                dumpToPile();
+                ObservableList<T> get = items.get();
+                for (int i = 0; i < get.size(); i++) {
+                    var listCell = getAvailableCell(i);
+                    listCell.owner().set(this);
+                    listCell.index().set(i);
+                    cells.addLast(listCell);
+                }
+                lastCellCount = observableItems().optional().map(List::size).orElse(-1);
+            }
+        }
+        contentChildren.addAll(cells);
+        super.layoutChildren();
     }
 }
