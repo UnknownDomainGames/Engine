fb54a5b7ec6ce52fd2a292c974d14ef300211ccb
Mouse <mouse0w0@qq.com>
2020-06-07 00:09:01 +0800

    fix: fix InvalidNameException message

--- game-engine/src/main/java/engine/registry/InvalidNameException.java
+++ game-engine/src/main/java/engine/registry/InvalidNameException.java
@@ -3,6 +3,6 @@ package engine.registry;
 public final class InvalidNameException extends RuntimeException {
 
     public InvalidNameException(String name) {
-        super("Name " + name + "is invalid");
+        super("Name " + name + " is invalid");
     }
 }
