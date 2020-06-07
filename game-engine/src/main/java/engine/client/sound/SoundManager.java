685329ea7ebed45397e96db70dfcf5f78903aa3b
Mouse <mouse0w0@qq.com>
2020-06-07 14:46:09 +0800

    feat: change getSoundSource return value to SoundSource

--- game-engine/src/main/java/engine/client/sound/SoundManager.java
+++ game-engine/src/main/java/engine/client/sound/SoundManager.java
@@ -1,11 +1,9 @@
 package engine.client.sound;
 
-import com.github.mouse0w0.observable.value.MutableObjectValue;
-
 public interface SoundManager {
     SoundSource createSoundSource(String name);
 
-    MutableObjectValue<SoundSource> getSoundSource(String name);
+    SoundSource getSoundSource(String name);
 
     SoundListener getListener();
 }
