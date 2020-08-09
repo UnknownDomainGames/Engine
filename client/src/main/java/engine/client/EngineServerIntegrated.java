4264d75b395159e1eb0a2c54911f762d6e0c55d9
cvrunmin <cvrunmin@outlook.com>
2020-08-09 18:03:39 +0800

    fix: fix duplicate logging

--- client/src/main/java/engine/client/EngineServerIntegrated.java
+++ client/src/main/java/engine/client/EngineServerIntegrated.java
@@ -1,32 +1,43 @@
 package engine.client;
 
-import engine.EngineBase;
 import engine.client.game.GameClient;
+import engine.event.EventBus;
 import engine.event.engine.EngineEvent;
 import engine.game.Game;
 import engine.game.GameData;
 import engine.game.GameServerFullAsync;
 import engine.logic.Ticker;
-import engine.mod.impl.EngineModManager;
+import engine.mod.ModManager;
+import engine.registry.RegistryManager;
 import engine.server.EngineServer;
 import engine.server.ServerConfig;
 import engine.server.network.NetworkServer;
-import engine.util.CrashHandlerImpl;
+import engine.util.CrashHandler;
+import engine.util.RuntimeEnvironment;
 import engine.util.Side;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
-public class EngineServerIntegrated extends EngineBase implements EngineServer {
+public class EngineServerIntegrated implements EngineServer {
+
+    private final Logger logger = LoggerFactory.getLogger("Integrated Server");
+    protected final List<Runnable> shutdownListeners = new LinkedList<>();
     private final EngineClient parent;
     private Thread serverThread;
     private Ticker ticker;
     private NetworkServer nettyServer;
     private ServerConfig serverConfig;
     private Game game;
+    private boolean initialized;
+    private boolean running;
+    private boolean markedTermination;
 
     public EngineServerIntegrated(EngineClient parent, ServerConfig serverConfig) {
-        super(parent.getRunPath());
         this.parent = parent;
         this.serverConfig = serverConfig;
     }
@@ -42,29 +53,69 @@ public class EngineServerIntegrated extends EngineBase implements EngineServer {
     }
 
     @Override
+    public Path getRunPath() {
+        return parent.getRunPath();
+    }
+
+    @Override
+    public Logger getLogger() {
+        return logger; //TODO: redirect to EngineClient's?
+    }
+
+    @Override
+    public RuntimeEnvironment getRuntimeEnvironment() {
+        return parent.getRuntimeEnvironment();
+    }
+
+    @Override
     public Side getSide() {
         return Side.SERVER;
     }
 
     @Override
+    public EventBus getEventBus() {
+        return parent.getEventBus();
+    }
+
+    @Override
+    public RegistryManager getRegistryManager() {
+        return parent.getRegistryManager();
+    }
+
+    @Override
+    public ModManager getModManager() {
+        return parent.getModManager();
+    }
+
+    @Override
+    public CrashHandler getCrashHandler() {
+        return parent.getCrashHandler();
+    }
+
+    @Override
+    public void initEngine() {
+        if (initialized) {
+            throw new IllegalStateException("Engine has been initialized.");
+        }
+        initialized = true;
+        constructionStage();
+        modStage();
+        finishStage();
+    }
+
+
     protected void constructionStage() {
         logger.info("Initializing engine!");
-
-        crashHandler = (CrashHandlerImpl) parent.getCrashHandler();
-
-        eventBus = parent.getEventBus();
-        registryManager = parent.getRegistryManager();
-        modManager = (EngineModManager) parent.getModManager();
 
         serverThread = Thread.currentThread();
     }
 
-    @Override
+
     protected void modStage() {
 
     }
 
-    @Override
+
     protected void finishStage() {
         ticker = new Ticker(this::serverTick, partial -> {
         }, 20);
@@ -72,12 +123,15 @@ public class EngineServerIntegrated extends EngineBase implements EngineServer {
 
     @Override
     public void runEngine() {
-        super.runEngine();
+        if (running) {
+            throw new IllegalStateException("Engine is running.");
+        }
+        running = true;
 
         addShutdownListener(ticker::stop);
 
         logger.info("Finishing initialization!");
-        eventBus.post(new EngineEvent.Ready(this));
+        getEventBus().post(new EngineEvent.Ready(this));
         nettyServer = new NetworkServer();
         nettyServer.prepareNetworkEventBus();
         // NOTE: delay server binding to client connection
@@ -124,7 +178,7 @@ public class EngineServerIntegrated extends EngineBase implements EngineServer {
             game.terminate();
         }
 
-        eventBus.post(new EngineEvent.PreTermination(this));
+        getEventBus().post(new EngineEvent.PreTermination(this));
 
         ticker.stop();
         shutdownListeners.forEach(Runnable::run);
@@ -138,5 +192,26 @@ public class EngineServerIntegrated extends EngineBase implements EngineServer {
     @Override
     public boolean isPlaying() {
         return game != null && game.isReadyToPlay() && !game.isTerminated();
+    }
+
+    @Override
+    public void terminate() {
+        if (markedTermination) {
+            return;
+        }
+
+        markedTermination = true;
+        getEventBus().post(new EngineEvent.MarkedTermination(this));
+        logger.info("Marked engine terminated!");
+    }
+
+    @Override
+    public boolean isMarkedTermination() {
+        return markedTermination;
+    }
+
+    @Override
+    public void addShutdownListener(Runnable runnable) {
+        shutdownListeners.add(runnable);
     }
 }
