038a72633ca430c74b7cff8f241372b34b9ae06c
xtex <zbwxtex@qq.com>
2021-11-05 19:56:29 +0800

    feat(game-engine): add SynchronizableRegistry for ItemRegistry

--- game-engine/src/main/java/engine/registry/game/ItemRegistry.java
+++ game-engine/src/main/java/engine/registry/game/ItemRegistry.java
@@ -5,11 +5,12 @@ import engine.block.state.BlockState;
 import engine.item.BlockItem;
 import engine.item.Item;
 import engine.registry.Registry;
+import engine.registry.SynchronizableRegistry;
 import engine.server.network.packet.PacketSyncRegistry;
 
 import java.util.Optional;
 
-public interface ItemRegistry extends Registry<Item> {
+public interface ItemRegistry extends SynchronizableRegistry<Item> {
 
     Optional<BlockItem> getBlockItem(BlockState block);
 
